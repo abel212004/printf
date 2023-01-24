e9851596ec4d9c5c2cc6c7dd3b95505ad9984348
ethioendu <117819326+ethioendu@users.noreply.github.com>
2023-01-24 20:09:51 +0300

    Update _printf.c

--- _printf.c
+++ _printf.c
@@ -7,6 +7,7 @@ void print_buffer(char buffer[], int *buff_ind);
  * @format: format.
  * Return: Printed chars.
  */
+
 int _printf(const char *format, ...)
 {
 	int i, printed = 0, printed_chars = 0;
@@ -29,6 +30,7 @@ int _printf(const char *format, ...)
 			/* write(1, &format[i], 1);*/
 			printed_chars++;
 		}
+		
 		else
 		{
 			print_buffer(buffer, &buff_ind);
@@ -57,10 +59,13 @@ int _printf(const char *format, ...)
  * @buffer: Array of chars
  * @buff_ind: Index at which to add next char, represents the length.
  */
+
 void print_buffer(char buffer[], int *buff_ind)
 {
+	
 	if (*buff_ind > 0)
 		write(1, &buffer[0], *buff_ind);
 
 	*buff_ind = 0;
+	
 }
