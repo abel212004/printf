953a228bf8cb75ee2e61c857b873eabf00c6dd96
ethioendu <117819326+ethioendu@users.noreply.github.com>
2023-01-24 20:27:19 +0300

    Update function.c

--- function.c
+++ function.c
@@ -3,7 +3,7 @@
 /************************* PRINT CHAR *************************/
 
 /**
- * print_char - Prints a char
+ * print_char - Act as Prints a char
  * @types: List a of arguments
  * @buffer: Buffer array to handle print
  * @flags:  Calculates active flags
@@ -30,6 +30,7 @@ int print_char(va_list types, char buffer[],
  * @size: Size specifier
  * Return: Number of chars printed
  */
+
 int print_string(va_list types, char buffer[],
 	int flags, int width, int precision, int size)
 {
